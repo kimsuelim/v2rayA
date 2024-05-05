ab10f7b7b29ac21b2dc2d0e06f1cc57125ba4986
Surim Kim <kimsuelim@gmail.com>
2024-05-05 11:19:29 +0900

    Change dev api host

--- service/conf/variable.go
+++ service/conf/variable.go
@@ -9,7 +9,7 @@ var (
 	BasicAuthPassword        = "my_password"
 	AdminUsername            = "admin@admin"
 	AdminPassword            = "admin"
-	ApiHost                  = "http://host.docker.internal:8080"
+	ApiHost                  = "http://host.docker.internal:3000"
 	SentryDSN                = "https://5d1d8b27004195f9a0d9f996165b9a96@o4507083982700544.ingest.us.sentry.io/4507084478939136"
 	Version                  = "debug"
 	FoundNew                 = false
